1b74db00edb06943378605ebe89b8b553e5fcb3d
DESKTOP-8EKGJSR\eXsmobile_PC <exsmobile.dev2@gmail.com>
2021-01-14 20:17:33 +0900

    2021.01.14(목)  * jpa 프로젝트 첫 커밋

---
+++ src/main/java/kr/co/springJpaPosts/SpringJpaPostsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.springJpaPosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaPostsApplication.class, args);
+	}
+
+}
