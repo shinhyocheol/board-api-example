8a319ee61310545906c3461290e48c62c998eb70
eXs-mobile <zzzskssk1@exs-mobile.com>
2021-06-18 16:48:45 +0900

    2021.06.18(금)  * 생성자 선언을 돕기위한 어노테이션 추가

--- src/main/java/kr/co/platform/api/sign/dto/LoginDto.java
+++ src/main/java/kr/co/platform/api/sign/dto/LoginDto.java
@@ -1,8 +1,10 @@
 package kr.co.platform.api.sign.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 import kr.co.platform.api.member.domain.entity.Members;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +12,11 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginDto {
 
 	@NotBlank(message = "'email' is a required input value")
+	@Email(message = "It is not in email format")
 	private String email;
 
 	@NotBlank(message = "'password' is a required input value")
